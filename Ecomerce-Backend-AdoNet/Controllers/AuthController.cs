ab54179161eb69dccea1e9a03c5275d70b6a5023
dipankar <dipankargarai420@gmail.com>
2024-09-23 11:22:16 +0530

    Add project files.

---
+++ Ecomerce-Backend-AdoNet/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecomerce_Backend_AdoNet.Controllers
+{
+	public class AuthController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
